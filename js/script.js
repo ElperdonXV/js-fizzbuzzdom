29151c62566c69a62d6deb98fe2ac4a24fcbbb3b
ElperdonXV <davidemartinografico@gmail.com>
2021-11-23 15:13:40 +0100

    work on cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100.
+for (let index=1; index<=100; index++) {
+    console.log(index);
+}
 
 //MILESTONE 1
 //Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
